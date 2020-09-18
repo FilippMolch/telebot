d686e3facf45c4941d1d53a4de7424ba780fdfa3
FilippMolch <filippmail88@mail.ru>
2020-09-18 23:29:14 +0000

    oaahahha

--- bot.py
+++ bot.py
@@ -21,28 +21,34 @@ def upd_rasp(ahaha):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     try:
+
         if message.text.lower() == '/rasp':
             bot.send_message(message.chat.id, get_rasp())
         if message.text.lower()[0:7] == '/update':
-            text = message.text.lower().replace("/update", "")
-            upd_rasp(text)
-            print(text)
+            if message.from_user.username == "nesteroff561" or message.from_user.username == "FIl_OS":
+                text = message.text.lower().replace("/update", "")
+                upd_rasp(text)
+            else:
+                bot.send_message(message.chat.id, "permission denied")
         if message.text.lower()[0:5] == '/exec':
-            print("yes")
             comand = message.text.lower().replace("/exec", "").replace("&&", ">> file &&")
 
             for i in range(0, len(comand)):
                 print(i)
-
             comand = comand + " >> file"
 
-            print(comand)
-            exit_status = os.system(comand)
+            if message.from_user.username == "nesteroff561" or message.from_user.username == "FIl_OS":
+                exit_status = os.system(comand)
+            else:
+                bot.send_message(message.chat.id, "permission denied")
+
             bot.send_message(message.chat.id, get_file())
             os.remove("file")
+        if message.text.lower() == '/help':
+            bot.send_message(message.chat.id, "/exec - выполнить команду (доступно только админам)\n/rasp получить расписание\n/update заменить расписание (доступно только админам)")
 
     except Exception as e:
         print("ooops", e)
-        bot.send_message(message.chat.id, "ooops:\nfile empty")
+        bot.send_message(message.chat.id, "ooops")
 
 bot.polling()
